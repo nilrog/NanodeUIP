0960f037430b2ad4f2cb9e6eb84a29252ca087ab
Roger Tornstrom <roger@nilrogsplace.se>
2013-04-18 10:42:17 +0200

    Changed uip_log* to map to printf*

--- platform.cpp
+++ platform.cpp
@@ -8,6 +8,9 @@
   #include <WProgram.h> // Arduino 0022
 #endif
 
+#define uip_log printf
+#define uip_log_P printf_P
+
 void clock_init(void) {}
 
 clock_time_t clock_time(void) {
@@ -15,13 +18,3 @@ clock_time_t clock_time(void) {
 }
 
 void nullproc(void) {}
-
-extern void nanode_log(char *msg);
-extern void nanode_log_P(PGM_P msg);
-
-void uip_log(char *msg) {
-  nanode_log(msg);
-}
-void uip_log_P(PGM_P msg) {
-  nanode_log_P(msg);
-}
